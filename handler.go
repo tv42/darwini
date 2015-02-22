327cc6c33a4a18fb1a2df6ef15b22683c78a28a3
Tommi Virtanen <tv@eagain.net>
2015-02-22 15:43:53 -0800

    WithContext and NoContext connect mux.Handlers and net/http.Handlers

--- handler.go
+++ handler.go
@@ -20,3 +20,52 @@ var _ Handler = HandlerFunc(nil)
 func (fn HandlerFunc) ServeHTTP(ctx context.Context, w http.ResponseWriter, req *http.Request) {
 	fn(ctx, w, req)
 }
+
+// WithContext adapts a darwini.Handler to work as a net/http.Handler.
+// The context will be canceled when the HTTP client disconnects
+// early.
+func WithContext(h Handler) http.Handler {
+	return withContext{h}
+}
+
+type withContext struct {
+	Handler
+}
+
+var _ http.Handler = withContext{}
+
+func (h withContext) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	if n, ok := w.(http.CloseNotifier); ok {
+		gone := n.CloseNotify()
+		go func() {
+			select {
+			case <-ctx.Done():
+				return
+			case <-gone:
+				cancel()
+			}
+		}()
+	}
+
+	h.Handler.ServeHTTP(ctx, w, req)
+}
+
+// NoContext adapts a net/http Handler to work as a darwini.Handler.
+// Context deadlines or cancellation are not respected.
+func NoContext(h http.Handler) Handler {
+	return noContext{h}
+}
+
+type noContext struct {
+	http.Handler
+}
+
+var _ Handler = noContext{}
+
+func (h noContext) ServeHTTP(ctx context.Context, w http.ResponseWriter, req *http.Request) {
+	h.Handler.ServeHTTP(w, req)
+}
